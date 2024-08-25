d0e5c26f20a8d6c8d66f676493f293ce372112fb
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-08-25 21:10:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+	const charCount = {};
+    for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+	
+  for (let i = 0; i < str.length; i++) {
+    if (charCount[str[i]] === 1) {
+      return str[i];
+    }
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
